5e8b7b8ec057d72abfcaf1c2524d46593cfe6c12
Chris Patuzzo <chris@patuzzo.co.uk>
2015-11-24 18:44:37 +0000

    Add a Registry

---
+++ spec/sentient/registrySpec.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var describedClass = require("../../lib/sentient/registry");
+
+describe("Registry", function () {
+  var subject;
+
+  beforeEach(function () {
+    subject = new describedClass();
+  });
+
+  it("behaves as expected", function () {
+    expect(subject.nextLiteral()).toEqual(1);
+    expect(subject.nextLiteral()).toEqual(2);
+    expect(subject.nextLiteral()).toEqual(3);
+
+    expect(subject.nextSymbol()).toEqual("$$$_TMP1_$$$");
+    expect(subject.nextSymbol()).toEqual("$$$_TMP2_$$$");
+    expect(subject.nextSymbol()).toEqual("$$$_TMP3_$$$");
+  });
+});
