a55dbe27c7382c37d1394480a7cd90f823b03772
Chris Patuzzo <chris@patuzzo.co.uk>
2015-12-28 18:04:31 +0000

    Add the ‘add’ instruction

--- spec/sentient/compiler/level2Compiler/instructionSetSpec.js
+++ spec/sentient/compiler/level2Compiler/instructionSetSpec.js
@@ -518,4 +518,301 @@ describe("InstructionSet", function () {
       });
     });
   });
+
+  describe("add", function () {
+    describe("for integers of equal width", function () {
+      beforeEach(function () {
+        stack.push("bottom");
+        stack.push("a");
+        stack.push("b");
+
+        symbolTable.set("bottom", "anything", ["anything"]);
+        symbolTable.set("a", "integer", ["foo", "bar"]);
+        symbolTable.set("b", "integer", ["baz", "qux"]);
+      });
+
+      it("replaces the top two symbols for one symbol on the stack", function () {
+        subject.add();
+        expect(stack.pop()).toEqual("$$$_TMP1_$$$");
+        expect(stack.pop()).toEqual("bottom");
+      });
+
+      it("adds the new symbol to the symbol table", function () {
+        subject.add();
+        var newSymbol = stack.pop();
+
+        expect(symbolTable.type(newSymbol)).toEqual("integer");
+        expect(symbolTable.symbols(newSymbol)).toEqual([
+          "$$$_INTEGER1_BIT0_$$$",
+          "$$$_INTEGER1_BIT1_$$$",
+          "$$$_INTEGER1_BIT2_$$$"
+        ]);
+      });
+
+      it("writes instructions for 'add'", function () {
+        spyOn(codeWriter, "instruction");
+        subject.add();
+
+        // === bit 2 ===
+
+        // c_in = false
+        expect(codeWriter.instruction.calls.argsFor(0)).toEqual([
+          { type: "false" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(1)).toEqual([
+          { type: "pop", symbol: "$$$_BOOLEAN1_$$$" }
+        ]);
+
+        // l xor r
+        expect(codeWriter.instruction.calls.argsFor(2)).toEqual([
+          { type: "push", symbol: "bar" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(3)).toEqual([
+          { type: "push", symbol: "qux" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(4)).toEqual([
+          { type: "not" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(5)).toEqual([
+          { type: "equal" }
+        ]);
+
+        // sum = (l xor r) xor c_in
+        expect(codeWriter.instruction.calls.argsFor(6)).toEqual([
+          { type: "push", symbol: "$$$_BOOLEAN1_$$$" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(7)).toEqual([
+          { type: "not" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(8)).toEqual([
+          { type: "equal" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(9)).toEqual([
+          { type: "pop", symbol: "$$$_INTEGER1_BIT2_$$$" }
+        ]);
+
+        // l xor r
+        expect(codeWriter.instruction.calls.argsFor(10)).toEqual([
+          { type: "push", symbol: "bar" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(11)).toEqual([
+          { type: "push", symbol: "qux" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(12)).toEqual([
+          { type: "not" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(13)).toEqual([
+          { type: "equal" }
+        ]);
+
+        // (l xor r) and c_in
+        expect(codeWriter.instruction.calls.argsFor(14)).toEqual([
+          { type: "push", symbol: "$$$_BOOLEAN1_$$$" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(15)).toEqual([
+          { type: "and" }
+        ]);
+
+        // a and b
+        expect(codeWriter.instruction.calls.argsFor(16)).toEqual([
+          { type: "push", symbol: "bar" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(17)).toEqual([
+          { type: "push", symbol: "qux" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(18)).toEqual([
+          { type: "and" }
+        ]);
+
+        // carry = (a and b) or ((l xor r) and c_in)
+        expect(codeWriter.instruction.calls.argsFor(19)).toEqual([
+          { type: "or" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(20)).toEqual([
+          { type: "pop", symbol: "$$$_BOOLEAN2_$$$" }
+        ]);
+
+        // === bit 1 ===
+
+        // l xor r
+        expect(codeWriter.instruction.calls.argsFor(21)).toEqual([
+          { type: "push", symbol: "foo" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(22)).toEqual([
+          { type: "push", symbol: "baz" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(23)).toEqual([
+          { type: "not" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(24)).toEqual([
+          { type: "equal" }
+        ]);
+
+        // sum = (l xor r) xor c_in
+        expect(codeWriter.instruction.calls.argsFor(25)).toEqual([
+          { type: "push", symbol: "$$$_BOOLEAN2_$$$" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(26)).toEqual([
+          { type: "not" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(27)).toEqual([
+          { type: "equal" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(28)).toEqual([
+          { type: "pop", symbol: "$$$_INTEGER1_BIT1_$$$" }
+        ]);
+
+        // l xor r
+        expect(codeWriter.instruction.calls.argsFor(29)).toEqual([
+          { type: "push", symbol: "foo" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(30)).toEqual([
+          { type: "push", symbol: "baz" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(31)).toEqual([
+          { type: "not" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(32)).toEqual([
+          { type: "equal" }
+        ]);
+
+        // (l xor r) and c_in
+        expect(codeWriter.instruction.calls.argsFor(33)).toEqual([
+          { type: "push", symbol: "$$$_BOOLEAN2_$$$" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(34)).toEqual([
+          { type: "and" }
+        ]);
+
+        // a and b
+        expect(codeWriter.instruction.calls.argsFor(35)).toEqual([
+          { type: "push", symbol: "foo" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(36)).toEqual([
+          { type: "push", symbol: "baz" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(37)).toEqual([
+          { type: "and" }
+        ]);
+
+        // c_in = (a and b) or ((l xor r) and c_in)
+        expect(codeWriter.instruction.calls.argsFor(38)).toEqual([
+          { type: "or" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(39)).toEqual([
+          { type: "pop", symbol: "$$$_BOOLEAN3_$$$" }
+        ]);
+
+        // === bit 0 ===
+
+        // a == b
+        expect(codeWriter.instruction.calls.argsFor(40)).toEqual([
+          { type: "push", symbol: "foo" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(41)).toEqual([
+          { type: "push", symbol: "baz" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(42)).toEqual([
+          { type: "equal" }
+        ]);
+
+        // (a == b) && c_in
+        expect(codeWriter.instruction.calls.argsFor(43)).toEqual([
+          { type: "push", symbol: "$$$_BOOLEAN3_$$$" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(44)).toEqual([
+          { type: "and" }
+        ]);
+
+        // a == !b
+        expect(codeWriter.instruction.calls.argsFor(45)).toEqual([
+          { type: "push", symbol: "foo" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(46)).toEqual([
+          { type: "push", symbol: "baz" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(47)).toEqual([
+          { type: "not" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(48)).toEqual([
+          { type: "equal" }
+        ]);
+
+        // (a == !b) && !c_in
+        expect(codeWriter.instruction.calls.argsFor(49)).toEqual([
+          { type: "push", symbol: "$$$_BOOLEAN3_$$$" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(50)).toEqual([
+          { type: "not" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(51)).toEqual([
+          { type: "and" }
+        ]);
+
+        // sum = ((a == b) && c_in) || ((a == !b) && !c_in)
+        expect(codeWriter.instruction.calls.argsFor(52)).toEqual([
+          { type: "or" }
+        ]);
+
+        expect(codeWriter.instruction.calls.argsFor(53)).toEqual([
+          { type: "pop", symbol: "$$$_INTEGER1_BIT0_$$$" }
+        ]);
+
+        expect(codeWriter.instruction.calls.count()).toEqual(54);
+      });
+    });
+
+    describe("for non-integer types", function () {
+      beforeEach(function () {
+        stack.push("bottom");
+        stack.push("a");
+        stack.push("b");
+
+        symbolTable.set("bottom", "anything", ["anything"]);
+        symbolTable.set("a", "boolean", ["foo"]);
+        symbolTable.set("b", "boolean", ["bar"]);
+      });
+
+      it("throws an error", function () {
+        expect(function () {
+          subject.add();
+        }).toThrow();
+      });
+    });
+  });
 });
