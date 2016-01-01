0bbfac8348e9c9a92cb949340373d08da76635d2
Chris Patuzzo <chris@patuzzo.co.uk>
2016-01-01 15:04:32 +0000

    Level 1 integration test: and

---
+++ spec/sentient/integration/level1/andSpec.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var compiler = "../../../../lib/sentient/compiler";
+var Level1Compiler = require(compiler + "/level1Compiler");
+
+var runtime = "../../../../lib/sentient/runtime";
+var Level1Runtime = require(runtime + "/level1Runtime");
+
+var Machine = require("../../../../lib/sentient/machine");
+
+var _ = require("underscore");
+
+describe("Integration: 'and'", function () {
+  it("produces the correct result for 'false && true'", function () {
+    var program = Level1Compiler.compile({
+      instructions: [
+        { type: "false" },
+        { type: "true" },
+        { type: "and" },
+        { type: "pop", symbol: "a" },
+        { type: "variable", symbol: "a" }
+      ]
+    });
+
+    var assignments = Level1Runtime.encode(program, {});
+    var result = Machine.run(program, assignments);
+    result = Level1Runtime.decode(program, result);
+    expect(result.a).toEqual(false);
+  });
+
+  it("produces the correct result for 'true && true'", function () {
+    var program = Level1Compiler.compile({
+      instructions: [
+        { type: "true" },
+        { type: "true" },
+        { type: "and" },
+        { type: "pop", symbol: "a" },
+        { type: "variable", symbol: "a" }
+      ]
+    });
+
+    var assignments = Level1Runtime.encode(program, {});
+    var result = Machine.run(program, assignments);
+    result = Level1Runtime.decode(program, result);
+    expect(result.a).toEqual(true);
+  });
+
+  it("throws an error if there are fewer than two stack symbols", function () {
+    expect(function () {
+      Level1Compiler.compile({
+        instructions: [
+          { type: "true" },
+          { type: "and" }
+        ]
+      });
+    }).toThrow();
+  });
+});
