381f0b6b3730d324d51ac206aaa9d809eb45697e
Chris Patuzzo <chris@patuzzo.co.uk>
2016-01-01 14:54:22 +0000

    Level 1 integration test: variable

---
+++ spec/sentient/integration/level1/variableSpec.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var compiler = "../../../../lib/sentient/compiler";
+var Level1Compiler = require(compiler + "/level1Compiler");
+
+var runtime = "../../../../lib/sentient/runtime";
+var Level1Runtime = require(runtime + "/level1Runtime");
+
+var Machine = require("../../../../lib/sentient/machine");
+
+var _ = require("underscore");
+
+describe("Integration: 'variable'", function () {
+  it("only exposes the specified variables", function () {
+    var program = Level1Compiler.compile({
+      instructions: [
+        { type: "push", symbol: "a" },
+        { type: "true" },
+        { type: "push", symbol: "b" },
+        { type: "false" },
+        { type: "pop", symbol: "c" },
+        { type: "push", symbol: "d" },
+
+        { type: "variable", symbol: "b" },
+        { type: "variable", symbol: "c" }
+      ]
+    });
+
+    var assignments = Level1Runtime.encode(program, {});
+
+    var result = Machine.run(program, assignments);
+    result = Level1Runtime.decode(program, result);
+
+    expect(_.keys(result)).toEqual(["b", "c"]);
+  });
+
+  it("throws an error if the variable does not exist", function () {
+    expect(function () {
+      Level1Compiler.compile({
+        instructions: [
+          { type: "variable", symbol: "missing" }
+        ]
+      });
+    }).toThrow();
+  });
+});
