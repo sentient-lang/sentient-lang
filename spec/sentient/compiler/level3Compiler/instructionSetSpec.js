fe6d90fe579ffbb7479a57bb1129ac263177f52d
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-05 12:50:07 +0000

    Add the ‘boolean’ and ‘integer’ instructions

---
+++ spec/sentient/compiler/level3Compiler/instructionSetSpec.js
@@ -0,0 +1,102 @@
+"use strict";
+
+var compiler = "../../../../lib/sentient/compiler";
+
+var SpecHelper = require("../../../specHelper");
+var describedClass = require(compiler + "/level3Compiler/instructionSet");
+var Stack = require(compiler + "/common/stack");
+var SymbolTable = require(compiler + "/common/symbolTable");
+var Registry = require(compiler + "/level3Compiler/registry");
+var CodeWriter = require(compiler + "/level3Compiler/codeWriter");
+
+describe("InstructionSet", function () {
+  var subject, stack, symbolTable, registry, codeWriter;
+
+  beforeEach(function () {
+    stack = new Stack();
+    symbolTable = new SymbolTable();
+    registry = new Registry();
+    codeWriter = new CodeWriter();
+
+    subject = new describedClass({
+      stack: stack,
+      symbolTable: symbolTable,
+      registry: registry,
+      codeWriter: codeWriter
+    });
+  });
+
+  describe("boolean", function () {
+    it("adds the boolean to the symbol table", function () {
+      subject._boolean("foo");
+      subject._boolean("bar");
+
+      expect(symbolTable.type("foo")).toEqual("boolean");
+      expect(symbolTable.symbols("foo")).toEqual(["$$$_L3_BOOLEAN1_$$$"]);
+
+      expect(symbolTable.type("bar")).toEqual("boolean");
+      expect(symbolTable.symbols("bar")).toEqual(["$$$_L3_BOOLEAN2_$$$"]);
+    });
+
+    it("writes instructions to register the boolean's symbol", function () {
+      spyOn(codeWriter, "instruction");
+      subject._boolean("foo");
+
+      expect(SpecHelper.calls(codeWriter.instruction)).toEqual([
+        { type: "boolean", symbol: "$$$_L3_BOOLEAN1_$$$" },
+      ]);
+    });
+
+    describe("when the boolean is already declared", function () {
+      beforeEach(function () {
+        symbolTable.set("foo", "anything", ["anything"]);
+      });
+
+      it("throws an error", function () {
+        expect(function () {
+          subject._boolean("foo");
+        }).toThrow();
+      });
+    });
+  });
+
+  describe("integer", function () {
+    it("adds the integer to the symbol table", function () {
+      subject._integer("foo", 3);
+      subject._integer("bar", 2);
+
+      expect(symbolTable.type("foo")).toEqual("integer");
+      expect(symbolTable.symbols("foo")).toEqual(["$$$_L3_INTEGER1_$$$"]);
+
+      expect(symbolTable.type("bar")).toEqual("integer");
+      expect(symbolTable.symbols("bar")).toEqual(["$$$_L3_INTEGER2_$$$"]);
+    });
+
+    it("writes instructions to register the integer's symbol", function () {
+      spyOn(codeWriter, "instruction");
+      subject._integer("foo", 3);
+
+      expect(SpecHelper.calls(codeWriter.instruction)).toEqual([
+        { type: "integer", symbol: "$$$_L3_INTEGER1_$$$", width: 3 },
+      ]);
+    });
+
+    it("throws an error if the width isn't provided", function () {
+      expect(function () {
+        subject._integer("foo");
+      }).toThrow();
+    });
+
+    describe("when the integer is already declared", function () {
+      beforeEach(function () {
+        symbolTable.set("foo", "anything", ["anything"]);
+      });
+
+      it("throws an error", function () {
+        expect(function () {
+          subject._integer("foo", 3);
+        }).toThrow();
+      });
+    });
+  });
+});
