86ff541cb89f32ba186fea784364b68e6890ee39
Chris Patuzzo <chris@patuzzo.co.uk>
2016-03-15 13:34:18 +0000

    Swap the palindome spec for subset sum

--- spec/computationclub/exercise4Spec.js
+++ spec/computationclub/exercise4Spec.js
@@ -9,36 +9,34 @@ describe("Exercise 4", function () {
     ]
   });
 
-  it("can find an array that is a palindrome of 3 numbers", function () {
-    var result = Sentient.run(machineCode, {});
+  it("can solve the subset sum problem", function () {
+    var result = Sentient.run(machineCode, {
+      numbers: [5, 8, 4, 11, 6],
+      total: 20
+    });
 
-    var palindrome = result.palindrome;
-    var reverse = palindrome.slice(0).reverse();
-
-    expect(palindrome).toEqual(reverse);
+    expect(result.members).toEqual([
+      true, false, true, true, false // 5, 4, 11
+    ]);
   });
 
-  it("can find a solution that starts with a 5", function () {
-    var result = Sentient.run(machineCode, { palindrome: { 0: 5 } });
-    var palindrome = result.palindrome;
+  it("can find a solution for 15", function () {
+    var result = Sentient.run(machineCode, {
+      numbers: [5, 8, 4, 11, 6],
+      total: 15
+    });
 
-    expect(palindrome[0]).toEqual(5);
-    expect(palindrome[2]).toEqual(5);
+    expect(result.members).toEqual([
+      true, false, true, false, true // 5, 4, 6
+    ]);
   });
 
-  it("finds no solution that starts with 5 and ends with 3", function () {
-    var result = Sentient.run(machineCode, { palindrome: { 0: 5, 2: 3 } });
+  it("does not find a solution for 7", function () {
+    var result = Sentient.run(machineCode, {
+      numbers: [5, 8, 4, 11, 6],
+      total: 7
+    });
 
     expect(result).toEqual({});
-  });
-
-  it("only finds solutions that multiply to equal a given number", function () {
-    var result = Sentient.run(machineCode, { total: 300 });
-    var palindrome = result.palindrome;
-    expect(palindrome[0] * palindrome[1] * palindrome[2]).toEqual(300);
-
-    result = Sentient.run(machineCode, { total: -120 });
-    palindrome = result.palindrome;
-    expect(palindrome[0] * palindrome[1] * palindrome[2]).toEqual(-120);
   });
 });
