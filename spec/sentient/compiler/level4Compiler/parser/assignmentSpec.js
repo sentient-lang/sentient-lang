68a73d0f22a275a982c7007e21db67a6732a32f0
Chris Patuzzo <chris@patuzzo.co.uk>
2016-03-25 16:58:57 +0000

    Parse multiple assignment

--- spec/sentient/compiler/level4Compiler/parser/assignmentSpec.js
+++ spec/sentient/compiler/level4Compiler/parser/assignmentSpec.js
@@ -13,11 +13,24 @@ describe("assignment", function () {
   });
 
   it("accepts valid", function () {
-    expect(subject.parse("a = b")).toEqual(["a", "b"]);
-    expect(subject.parse("a = true")).toEqual(["a", true]);
-    expect(subject.parse("a = b && c")).toEqual(["a", ["b", "c", "&&"]]);
-    expect(subject.parse("foo_1 = -5")).toEqual(["foo_1", [5, "-"]]);
-    expect(subject.parse("a = b == c")).toEqual(["a", ["b", "c", "=="]]);
+    expect(subject.parse("a = b")).toEqual([["a"], ["b"]]);
+    expect(subject.parse("a = true")).toEqual([["a"], [true]]);
+    expect(subject.parse("a = b && c")).toEqual([["a"], [["b", "c", "&&"]]]);
+    expect(subject.parse("foo_1 = -5")).toEqual([["foo_1"], [[5, "-"]]]);
+    expect(subject.parse("a = b == c")).toEqual([["a"], [["b", "c", "=="]]]);
+    expect(subject.parse("a, b = 1, 2")).toEqual([["a", "b"], [1, 2]]);
+
+    expect(subject.parse("a, b, c = 1, x, true")).toEqual(
+      [["a", "b", "c"], [1, "x", true]]
+    );
+
+    expect(subject.parse("a, b = x / 2, y + z")).toEqual(
+      [["a", "b"], [["x", 2, "/"], ["y", "z", "+"]]]
+    );
+
+    expect(subject.parse("a, b, c = 1, a + a, b * b")).toEqual(
+      [["a", "b", "c"], [1, ["a", "a", "+"], ["b", "b", "*"]]]
+    );
   });
 
   it("rejects invalid", function () {
@@ -28,6 +41,8 @@ describe("assignment", function () {
     expect(function () { subject.parse("a =b"); }).toThrow();
     expect(function () { subject.parse("a = b = c"); }).toThrow();
     expect(function () { subject.parse("a == b"); }).toThrow();
-    expect(function () { subject.parse("a = 1, 2"); }).toThrow();
+    expect(function () { subject.parse("1 = a"); }).toThrow();
+    expect(function () { subject.parse("a, = 1"); }).toThrow();
+    expect(function () { subject.parse("a, b = 1 1"); }).toThrow();
   });
 });
