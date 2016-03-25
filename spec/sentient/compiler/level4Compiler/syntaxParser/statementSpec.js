6f41bf1bf855a8c1560356db429730fb98f8aefe
Chris Patuzzo <chris@patuzzo.co.uk>
2016-03-25 17:54:42 +0000

    Parse statements

---
+++ spec/sentient/compiler/level4Compiler/syntaxParser/statementSpec.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var compiler = "../../../../../lib/sentient/compiler";
+var SyntaxParser = require(compiler + "/level4Compiler/syntaxParser");
+
+describe("statement", function () {
+  var subject;
+
+  beforeEach(function () {
+    subject = new SyntaxParser({
+      allowedStartRules: ["statement"]
+    });
+  });
+
+  it("parses declarations", function () {
+    expect(subject.parse("int6 a")).toEqual(
+      { type: "declaration", value: [["int", 6], ["a"]] }
+    );
+
+    expect(subject.parse("array3<int4> a, b")).toEqual(
+      { type: "declaration", value: [["array", 3, "int", 4], ["a", "b"]] }
+    );
+  });
+
+  it("parses assignments", function () {
+    expect(subject.parse("a = 1")).toEqual(
+      { type: "assignment", value: [["a"], [1]] }
+    );
+
+    expect(subject.parse("a, b = (1 + 2) / 3, true")).toEqual(
+      { type: "assignment", value: [["a", "b"], [[[1, 2, "+"], 3, "/"], true]] }
+    );
+  });
+
+  it("parses vary statements", function () {
+    expect(subject.parse("vary a")).toEqual(
+      { type: "vary", value: ["a"] }
+    );
+
+    expect(subject.parse("vary x0, x1, foo_bar")).toEqual(
+      { type: "vary", value: ["x0", "x1", "foo_bar"] }
+    );
+  });
+
+  it("parses invariant statements", function () {
+    expect(subject.parse("invariant true")).toEqual(
+      { type: "invariant", value: [true] }
+    );
+
+    expect(subject.parse("invariant a && b, b == true")).toEqual(
+      { type: "invariant", value: [["a", "b", "&&"], ["b", true, "=="]] }
+    );
+  });
+});
