35834ba0b84fb96d604ff844643dc4687e4ae306
Chris Patuzzo <chris@patuzzo.co.uk>
2016-05-05 23:41:29 +0100

    Test stdlib immutability

---
+++ spec/sentient/integration/level4/immutabilitySpec.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var Sentient = require("../../../../lib/sentient");
+
+describe("immutability", function () {
+  it("allows user-defined functions to be redefined", function () {
+    var program = Sentient.compile(" \n\
+      function foo () { return 1; }; \n\
+      function foo () { return 2; }; \n\
+                                     \n\
+      a = foo();                     \n\
+      vary a;                        \n\
+    ");
+    var result = Sentient.run(program);
+
+    expect(result).toEqual({ a: 2 });
+  });
+
+  it("does not allow standard library functions to be redefined", function () {
+    expect(function () {
+      Sentient.compile("function && () { return 1; };");
+    }).toThrow();
+
+    expect(function () {
+      Sentient.compile("function if () { return 1; };");
+    }).toThrow();
+
+    expect(function () {
+      Sentient.compile("function abs () { return 1; };");
+    }).toThrow();
+  });
+});
