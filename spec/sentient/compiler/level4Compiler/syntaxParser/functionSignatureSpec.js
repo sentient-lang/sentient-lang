25ea83c2bb2855e69a98137cbbecfd4daa2cb13b
Chris Patuzzo <chris@patuzzo.co.uk>
2016-05-05 14:17:16 +0100

    Parse dynamically scoped functions

--- spec/sentient/compiler/level4Compiler/syntaxParser/functionSignatureSpec.js
+++ spec/sentient/compiler/level4Compiler/syntaxParser/functionSignatureSpec.js
@@ -5,13 +5,23 @@ var subject = SpecHelper.parserForRule("functionSignature");
 
 describe("functionSignature", function () {
   it("accepts valid", function () {
-    expect(subject.parse("function foo ()")).toEqual(["foo", []]);
-    expect(subject.parse("function foo (a)")).toEqual(["foo", ["a"]]);
-    expect(subject.parse("function foo (a, b)")).toEqual(["foo", ["a", "b"]]);
-    expect(subject.parse("function double (x)")).toEqual(["double", ["x"]]);
-    expect(subject.parse("function x( a , b )")).toEqual(["x", ["a", "b"]]);
-    expect(subject.parse("function zero? (x)")).toEqual(["zero?", ["x"]]);
-    expect(subject.parse("function +(x, y)")).toEqual(["+", ["x", "y"]]);
+    expect(subject.parse("function foo ()")).toEqual(["foo", false, []]);
+    expect(subject.parse("function foo (a)")).toEqual(["foo", false, ["a"]]);
+    expect(subject.parse("function foo (a, b)")).toEqual(
+      ["foo", false, ["a", "b"]]
+    );
+    expect(subject.parse("function double (x)")).toEqual(
+      ["double", false, ["x"]]
+    );
+    expect(subject.parse("function x( a , b )")).toEqual(
+      ["x", false, ["a", "b"]]
+    );
+    expect(subject.parse("function zero? (x)")).toEqual(
+      ["zero?", false, ["x"]]
+    );
+    expect(subject.parse("function +(x, y)")).toEqual(["+", false, ["x", "y"]]);
+    expect(subject.parse("function^ incr ()")).toEqual(["incr", true, []]);
+    expect(subject.parse("function^ add (x)")).toEqual(["add", true, ["x"]]);
   });
 
   it("rejects invalid", function () {
--- spec/sentient/compiler/level4Compiler/syntaxParser/functionSpec.js
+++ spec/sentient/compiler/level4Compiler/syntaxParser/functionSpec.js
@@ -6,28 +6,56 @@ var subject = SpecHelper.parserForRule("function");
 describe("function", function () {
   it("accepts valid", function () {
     expect(subject.parse("function noop () {}")).toEqual({
-      name: "noop", args: [], body: [], ret: [0]
+      name: "noop",
+      dynamic: false,
+      args: [],
+      body: [],
+      ret: [0]
     });
 
     expect(subject.parse("function add (a, b) { return a + b; }")).toEqual({
-      name: "add", args: ["a", "b"], body: [], ret: [1, ["+", "a", "b"]]
+      name: "add",
+      dynamic: false,
+      args: ["a", "b"],
+      body: [],
+      ret: [1, ["+", "a", "b"]]
     });
 
     expect(subject.parse("function double(x){return x*2;}")).toEqual({
-      name: "double", args: ["x"], body: [], ret: [1, ["*", "x", 2]]
+      name: "double",
+      dynamic: false,
+      args: ["x"],
+      body: [],
+      ret: [1, ["*", "x", 2]]
     });
 
     expect(subject.parse("function zero? (x) { return x == 0; }")).toEqual({
-      name: "zero?", args: ["x"], body: [], ret: [1, ["==", "x", 0]]
+      name: "zero?",
+      dynamic: false,
+      args: ["x"],
+      body: [],
+      ret: [1, ["==", "x", 0]]
     });
 
     expect(
       subject.parse("function foo () { a = 1; b = 2; return3 a, b, 3; }")
     ).toEqual({
-      name: "foo", args: [], body: [
+      name: "foo",
+      dynamic: false,
+      args: [],
+      body: [
         { type: "assignment", value: [["a"], [1]] },
         { type: "assignment", value: [["b"], [2]] }
-      ], ret: [3, "a", "b", 3]
+      ],
+      ret: [3, "a", "b", 3]
+    });
+
+    expect(subject.parse("function^ double_x () { x *= 2; }")).toEqual({
+      name: "double_x",
+      dynamic: true,
+      args: [],
+      body: [{ type: "assignment", value: [["x"], [["*", "x", 2]]] }],
+      ret: [0]
     });
   });
 
@@ -36,5 +64,6 @@ describe("function", function () {
     expect(function () { subject.parse("functionfoo () {}"); }).toThrow();
     expect(function () { subject.parse("function () {}"); }).toThrow();
     expect(function () { subject.parse("function foo () {};"); }).toThrow();
+    expect(function () { subject.parse("function ^ foo () {}"); }).toThrow();
   });
 });
