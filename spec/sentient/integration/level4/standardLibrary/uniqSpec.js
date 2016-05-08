12f61970d6c3f9f327a1fd77bab0dc47ef23f85f
Chris Patuzzo <chris@patuzzo.co.uk>
2016-05-08 15:23:19 +0100

    Add ‘uniq?’ to standard library

---
+++ spec/sentient/integration/level4/standardLibrary/uniqSpec.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var Sentient = require("../../../../../lib/sentient");
+
+describe("standard library: uniq?", function () {
+  it("returns true if the array contains unique elements", function () {
+    var program = Sentient.compile("a = uniq?([1, 2, 3]); vary a;");
+    var result = Sentient.run(program);
+    expect(result).toEqual({ a: true });
+
+    program = Sentient.compile("a = uniq?([1, 2, 2]); vary a;");
+    result = Sentient.run(program);
+    expect(result).toEqual({ a: false });
+
+    program = Sentient.compile("a = uniq?([1, 1, 3]); vary a;");
+    result = Sentient.run(program);
+    expect(result).toEqual({ a: false });
+
+    program = Sentient.compile("a = uniq?([0, 0, 0]); vary a;");
+    result = Sentient.run(program);
+    expect(result).toEqual({ a: false });
+
+    program = Sentient.compile("a = uniq?([-1, 0, 1]); vary a;");
+    result = Sentient.run(program);
+    expect(result).toEqual({ a: true });
+
+    program = Sentient.compile("a = uniq?([true, false]); vary a;");
+    result = Sentient.run(program);
+    expect(result).toEqual({ a: true });
+
+    program = Sentient.compile("a = uniq?([true, true]); vary a;");
+    result = Sentient.run(program);
+    expect(result).toEqual({ a: false });
+
+    program = Sentient.compile("a = uniq?([[1], [2]]); vary a;");
+    result = Sentient.run(program);
+    expect(result).toEqual({ a: true });
+  });
+
+  it("can be called as a method instead of a function", function () {
+    var program = Sentient.compile("a = [-1, 0, 1].uniq?; vary a;");
+    var result = Sentient.run(program);
+    expect(result).toEqual({ a: true });
+  });
+});
