861edcb57314b5b274aedcaf9c7ce8b9f6fdc3d4
Chris Patuzzo <chris@patuzzo.co.uk>
2016-05-21 16:06:25 +0100

    Fix a bug with LingelingAdapter…
    Previously if a ‘v 0’ appeared on a line by itself
    in the solution, it would mistakenly return a 0
    literal back as part of the result. This caused
    lingeling to raise an error on subsequent solves
    when the ‘number’ option is set.

--- spec/sentient/machine/lingelingAdapterSpec.js
+++ spec/sentient/machine/lingelingAdapterSpec.js
@@ -44,4 +44,52 @@ describe("LingelingAdapter", function () {
       new describedClass("does_not_exist", "p cnf 1 1\n1 0").solve();
     }).toThrow();
   });
+
+  it("throws an error if lingeling writes to stderr", function () {
+    expect(function () {
+      describedClass.solve("\n\
+        p cnf 1 1           \n\
+        -0 1                \n\
+      ");
+    }).toThrow();
+  });
+
+  it("copes with an edge case where 'v 0' is on a line by itself", function () {
+    var result = describedClass.solve("\n\
+      p cnf 29 29 \n\
+      1 0         \n\
+      2 0         \n\
+      3 0         \n\
+      4 0         \n\
+      5 0         \n\
+      6 0         \n\
+      7 0         \n\
+      8 0         \n\
+      9 0         \n\
+      10 0        \n\
+      11 0        \n\
+      12 0        \n\
+      13 0        \n\
+      14 0        \n\
+      15 0        \n\
+      16 0        \n\
+      17 0        \n\
+      18 0        \n\
+      19 0        \n\
+      20 0        \n\
+      21 0        \n\
+      22 0        \n\
+      23 0        \n\
+      24 0        \n\
+      25 0        \n\
+      26 0        \n\
+      27 0        \n\
+      28 0        \n\
+      29 0        \n\
+    ");
+
+    for (var i = 0; i < result.length; i += 1) {
+      expect(result[i]).not.toEqual(0);
+    }
+  });
 });
