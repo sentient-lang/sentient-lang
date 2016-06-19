2265881264bf7ff87776c81efcd100f5775571b1
Chris Patuzzo <chris@patuzzo.co.uk>
2016-06-19 22:40:06 +0100

    Fix a failing test

--- spec/sentient/integration/level4/macros/fetchSpec.js
+++ spec/sentient/integration/level4/macros/fetchSpec.js
@@ -7,18 +7,15 @@ var _ = require("underscore");
 describe("macro: fetch", function () {
   it("calls fetchIndex if index is a known constant", function () {
     var instructions = Level4Compiler.compile("\n\
-      array4<int> n;                           \n\
-      total = n[2] + n[3];                     \n\
+      array10<int> n;                          \n\
+      total = n[7] + n[8];                     \n\
       expose n, total;                         \n\
     ").instructions;
 
     var fetchIndexInstructions = _.select(instructions, function (i) {
-      return i.type === "fetchIndex";
+      return i.type === "fetchIndex" && (i.index === 7 || i.index === 8);
     });
 
-    expect(fetchIndexInstructions).toEqual([
-      { type: "fetchIndex", index: 2 },
-      { type: "fetchIndex", index: 3 }
-    ]);
+    expect(fetchIndexInstructions.length).toEqual(2);
   });
 });
