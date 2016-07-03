03ee12a6588d0b4416bb4afff2f9c48f66edec19
Chris Patuzzo <chris@patuzzo.co.uk>
2016-07-03 10:29:32 +0100

    Ensure ‘countBy’ works with nils

--- spec/sentient/integration/level4/standardLibrary/countBySpec.js
+++ spec/sentient/integration/level4/standardLibrary/countBySpec.js
@@ -29,4 +29,17 @@ describe("standard library: countBy", function () {
     var result = Sentient.run({ program: program });
     expect(result).toEqual([{ a: 1 }]);
   });
+
+  it("works correctly with nils", function () {
+    var program = Sentient.compile("\n\
+      i = 0;                        \n\
+      arr = [[10], [20, 30]][i];    \n\
+      a = arr.countBy(*even?);      \n\
+      b = arr.countBy(*odd?);       \n\
+      expose a, b;                  \n\
+    ");
+
+    var result = Sentient.run({ program: program });
+    expect(result).toEqual([{ a: 1, b: 0 }]);
+  });
 });
