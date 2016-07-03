8709da748b4c8005652a90d8a5b17cd4ae264040
Chris Patuzzo <chris@patuzzo.co.uk>
2016-07-03 10:15:58 +0100

    Ensure ‘map’ and ’collect’ work with dynamic scope

--- spec/sentient/integration/level4/standardLibrary/mapSpec.js
+++ spec/sentient/integration/level4/standardLibrary/mapSpec.js
@@ -80,15 +80,37 @@ describe("standard library: map", function () {
   });
 
   it("has an alias 'collect'", function () {
-    var program = Sentient.compile("            \n\
-      mapped = [1, 2, 3].collect(function (e) { \n\
-        return 2 * e;                           \n\
-      });                                       \n\
-                                                \n\
-      expose mapped;                            \n\
+    var program = Sentient.compile("               \n\
+      collected = [1, 2, 3].collect(function (e) { \n\
+        return 2 * e;                              \n\
+      });                                          \n\
+                                                   \n\
+      expose collected;                            \n\
     ");
     var result = Sentient.run({ program: program });
 
-    expect(result).toEqual([{ mapped: [2, 4, 6] }]);
+    expect(result).toEqual([{ collected: [2, 4, 6] }]);
+  });
+
+  it("can works correctly with dynamically scoped functions", function () {
+    var program = Sentient.compile("            \n\
+      x = 123;                                  \n\
+                                                \n\
+      mapped = [1, 2, 3].map(function^ (e) {    \n\
+        return e + x;                           \n\
+      });                                       \n\
+                                                \n\
+      collected = [1, 2, 3].map(function^ (e) { \n\
+        return e + x;                           \n\
+      });                                       \n\
+                                                \n\
+      expose mapped, collected;                 \n\
+    ");
+    var result = Sentient.run({ program: program });
+
+    expect(result).toEqual([{
+      mapped: [124, 125, 126],
+      collected: [124, 125, 126]
+    }]);
   });
 });
